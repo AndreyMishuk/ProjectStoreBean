b3c35362a07a1439437a737af75d303b80ec1a5c
Andrey Mishuk <andreygim.am@gmail.com>
2017-12-17 00:17:56 +0300

    feature(added verification of name input)

--- src/main/java/net/service/projectstorebeans/web/validator/LoginValidator.java
+++ src/main/java/net/service/projectstorebeans/web/validator/LoginValidator.java
@@ -1,6 +1,8 @@
 package net.service.projectstorebeans.web.validator;
 
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -14,13 +16,50 @@ public class LoginValidator implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
 
-        if (value.toString().length() < 5) {
-            ResourceBundle bundle = ResourceBundle.getBundle("conf.messages",
-                    FacesContext.getCurrentInstance().getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage("login_length_error");
+        ResourceBundle bundle = ResourceBundle.getBundle("conf.messages",
+                FacesContext.getCurrentInstance().getViewRoot().getLocale());
+
+        try {
+            String valueString = value.toString().trim();
+
+            if (chekNameIsCorect(valueString)) {
+                throw new IllegalArgumentException(bundle.getString("login_name_incorect"));
+            }
+
+            if (!Character.isLetter(valueString.charAt(0))) {
+                throw new IllegalArgumentException(bundle.getString("login_input_error"));
+            }
+
+            if (valueString.length() < 3) {
+                throw new IllegalArgumentException(bundle.getString("login_length_error"));
+            }
+
+        } catch (IllegalArgumentException e) {
+            FacesMessage message = new FacesMessage(e.getMessage());
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message);
         }
+
+    }
+
+//    private Boolean chekFirstLiteral(String str) {
+//
+//        String firstSymbol = "" + str.charAt(0);
+//        Pattern pattern = Pattern.compile("(\\W|\\d|_)");
+//        Matcher matcher = pattern.matcher(firstSymbol);
+//
+//        return matcher.matches();
+//    }
+
+    private Boolean chekNameIsCorect(String str) {
+
+        String[] nameDataBase = {"root", "main"};
+        for (String value : nameDataBase) {
+            if (value.equals(str)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
